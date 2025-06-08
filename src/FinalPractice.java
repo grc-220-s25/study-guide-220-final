89ad5fbe853f8e5d158d5efb0e1ebd1391f8e25c
Johncarlo Perez <johncarloperez@Johncarlos-MacBook-Pro.local>
2025-06-07 22:03:26 -0700

    finished second study guide question

--- src/FinalPractice.java
+++ src/FinalPractice.java
@@ -33,6 +33,49 @@ public class FinalPractice {
 
         return sum;
     }
-
     // TODO: implement the rest of the study guide AND MAKE GOOD UNIT TESTS
+    public static int LargestOfLastThree(ListNode head) {
+        if(head == null) return 0;
+
+        ListNode current = head;
+
+        int firstLast = 0;
+        int secondLast = 0;
+        int last = 0;
+        int fourthLast = 0;
+        
+        int count = 0;
+        while(current != null) {
+            count++;
+            current = current.next;
+        }
+
+        current = head;
+        int max = Integer.MIN_VALUE;
+
+        if(count < 4) {
+            while(current != null) {
+            if(current.data > max) {
+                max = current.data;
+            }
+            current = current.next;
+        }
+        return max;
+
+    } else {
+
+        while(current != null) {
+
+            firstLast = secondLast;
+            secondLast = last;
+            last = fourthLast;
+            fourthLast = current.data;
+
+            current = current.next;
+        }
+       int maxOfFour = Math.max(Math.max(firstLast, secondLast), Math.max(last, fourthLast));
+       return maxOfFour;
+     }
+    }
 }
+
