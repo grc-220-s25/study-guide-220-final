2361f9a5dde4c5c2dcb514c6c27f4206b5d58e0a
Johncarlo Perez <johncarloperez@Johncarlos-MacBook-Pro.local>
2025-06-07 22:31:24 -0700

    add more junit test

--- src/FinalPracticeTest.java
+++ src/FinalPracticeTest.java
@@ -11,6 +11,18 @@ public class FinalPracticeTest {
     }
 
     // TODO: Make more tests for oddIndexSum
+    @Test
+    void testOddIndexSum_evenLengthList() {
+        ListNode list = new ListNode(2, new ListNode(4, new ListNode(6, new ListNode(8, new ListNode(10, new ListNode(12, new ListNode(14, new ListNode(16, new ListNode(18)))))))));
+        int actual = FinalPractice.oddIndexSum(list);
+        assertEquals(40, actual);
+    }
+    @Test
+    void testEvenIndexSum_nullLengthList() {
+        ListNode list = new ListNode(0);
+        int actual = FinalPractice.oddIndexSum(list);
+        assertEquals(0, actual);
+    }
 
     // TODO: Make thorough tests for ALL the questions on the study guide
     @Test
@@ -19,6 +31,7 @@ public class FinalPracticeTest {
         int actual = FinalPractice.LargestOfLastThree(list);
         assertEquals(55, actual);
     }
+    
     @Test
     void testForLargestFourValues() {
         ListNode list = new ListNode(4, new ListNode(8, new ListNode(15, new ListNode(16, new ListNode(23, new ListNode(55, new ListNode(22, new ListNode(29, new ListNode(34)))))))));
