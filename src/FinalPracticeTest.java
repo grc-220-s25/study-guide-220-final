65f7bb968c55aab292e1ab559144caf06a7b5a64
Iquer_Dough <206500499+DennysSaenz123@users.noreply.github.com>
2025-06-11 09:28:05 -0700

    Added tests for second method

--- src/FinalPracticeTest.java
+++ src/FinalPracticeTest.java
@@ -13,7 +13,7 @@ public class FinalPracticeTest {
     @Test
     public void testTwoElements() {
         ListNode test4 = new ListNode(5, new ListNode(20));
-        assertEquals(20, sumOddIndexes(test4));
+        assertEquals(20, FinalPractice.oddIndexSum(test4));
     }
 
     @Test
@@ -24,7 +24,7 @@ public class FinalPracticeTest {
                             new ListNode(0,
                             new ListNode(3,
                             new ListNode(0))))));
-        assertEquals(0, sumOddIndexes(test5));
+        assertEquals(0, FinalPractice.oddIndexSum(test5));
     }
 
     @Test
@@ -35,22 +35,67 @@ public class FinalPracticeTest {
                             new ListNode(5,
                             new ListNode(0,
                             new ListNode(3))))));
-        assertEquals(15, sumOddIndexes(test6));
+        assertEquals(15, FinalPractice.oddIndexSum(test6));
+    }
+    @Test
+    public void testExampleList() {
+        ListNode list = new ListNode(7,
+                            new ListNode(3,
+                            new ListNode(19,
+                            new ListNode(21,
+                            new ListNode(14,
+                            new ListNode(33,
+                            new ListNode(8,
+                            new ListNode(26,
+                            new ListNode(11,
+                            new ListNode(5))))))))));
+        assertEquals(26, FinalPractice.maxOfLastThree(list));
     }
 
-    // Helper method if not already imported
-    public static int sumOddIndexes(ListNode head) {
-        int index = 0, sum = 0;
-        ListNode current = head;
-        while (current != null) {
-            if (index % 2 == 1) {
-                sum += current.data;
-            }
-            current = current.next;
-            index++;
-        }
-        return sum;
+    @Test
+    public void testExactlyThreeElements() {
+        ListNode list = new ListNode(10,
+                            new ListNode(20,
+                            new ListNode(30)));
+        assertEquals(30, FinalPractice.maxOfLastThree(list));
     }
+
+    @Test
+    public void testNegativeNumbers() {
+        ListNode list = new ListNode(-5,
+                            new ListNode(-10,
+                            new ListNode(-3,
+                            new ListNode(-1,
+                            new ListNode(-20)))));
+        // last three: -3, -1, -20 => max = -1
+        assertEquals(-1, FinalPractice.maxOfLastThree(list));
+    }
+
+    @Test
+    public void testDuplicates() {
+        ListNode list = new ListNode(1,
+                            new ListNode(2,
+                            new ListNode(3,
+                            new ListNode(4,
+                            new ListNode(3,
+                            new ListNode(3))))));
+        // last three: 4, 3, 3 => max = 4
+        assertEquals(4, FinalPractice.maxOfLastThree(list));
+    }
+
+    @Test
+    public void testTooShortListReturnsNegativeOne() {
+    ListNode oneNode = new ListNode(42);
+    assertEquals(-1, FinalPractice.maxOfLastThree(oneNode));
+
+    ListNode twoNodes = new ListNode(10, new ListNode(20));
+    assertEquals(-1, FinalPractice.maxOfLastThree(twoNodes));
 }
+}
+
+
+
+
+
 
 
